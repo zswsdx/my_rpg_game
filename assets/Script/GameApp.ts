841ef497fbe1a24274815d2861bf9aff263d9956
zswsdx <zswsdx@126.com>
2024-03-26 11:43:44 +0800

    基础

--- assets/Script/GameApp.ts
+++ assets/Script/GameApp.ts
@@ -1,10 +1,27 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, assetManager, Component, instantiate, Node, Prefab } from 'cc';
+import { Login } from './UI_Ctrl/Login';
 const { ccclass, property } = _decorator;
 
 @ccclass('GameApp')
 export class GameApp extends Component {
     start() {
-        console.log("game start!!!")
+        console.log("start GameApp")
+        //加载login界面
+        assetManager.loadBundle('UI', (err, bundle) => {
+            if (err){
+                console.log(err)
+                return
+            }
+            bundle.load('Login',Prefab, (err, prefab) => {
+                if (err){
+                    console.log(err)
+                    return
+                }
+                let login = instantiate(prefab);
+                login.setParent(this.node)
+                login.addComponent(Login)
+            })
+        });
     }
 
     update(deltaTime: number) {
---
+++ assets/Script/UI_Ctrl/Login.ts
@@ -0,0 +1,39 @@
+import { _decorator, assert, assetManager, Component, instantiate, Node, Prefab } from 'cc';
+import { Main } from './Main';
+const { ccclass, property } = _decorator;
+
+@ccclass('Login')
+export class Login extends Component {
+    start() {
+        this.node.getChildByName("Btn_Login").on("click",this.login);
+
+        this.node.getChildByName("Btn_Reg").on("click", () => {
+            console.log("register")
+        });
+    }
+
+    update(deltaTime: number) {
+        
+    }
+
+    login(){
+        assetManager.loadBundle('UI', (err, bundle) => {
+            if (err){
+                console.log(err)
+                return
+            }
+            bundle.load('Main',Prefab, (err, prefab) => {
+                if (err){
+                    console.log(err)
+                    return
+                }
+                let main = instantiate(prefab);
+                main.setParent(this.node.parent)
+                main.addComponent(Main)
+                this.node.removeFromParent()
+            })
+        });
+    }
+}
+
+
